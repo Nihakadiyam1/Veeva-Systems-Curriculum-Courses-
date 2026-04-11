c5d7c13357ad851e092b680b1c14fe5985f14816
HARIKA KADIMI <harikakadiyam852@gmail.com>
2026-04-11 07:48:39 +0530

    Updated Commit: Precision API BDD AUTOMATION FRAMEWORK

--- PRECISION_API_BDD_AUTOMATION_FRAMEWORK/API_BDD_Automation_Framework/src/test/java/stepdefinitions/Hooks.java
+++ PRECISION_API_BDD_AUTOMATION_FRAMEWORK/API_BDD_Automation_Framework/src/test/java/stepdefinitions/Hooks.java
@@ -12,10 +12,8 @@ import utils.ExtentManager;
 
 public class Hooks {
 
-    // ✅ Make final (good practice)
     private static final ExtentReports extent = ExtentManager.getInstance();
 
-    // ❗ Do NOT make final
     private static ExtentTest test;
 
     @Before
